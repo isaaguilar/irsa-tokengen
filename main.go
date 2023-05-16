1518f82616926bced1630fef76e4aec30c3aa968
isaaguilar <isa@aguilarstory.com>
2023-05-16 09:34:03 -0400

    variablize the duration

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -21,6 +22,15 @@ func (i IdentityTokenRetriever) GetIdentityToken() ([]byte, error) {
 }
 
 func main() {
+	t_ := os.Getenv("IRSA_TOKENGEN_DURATION_IN_HOURS")
+	if t_ == "" {
+		t_ = "1"
+	}
+	t, err := strconv.Atoi(t_)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
 		log.Fatal(err)
@@ -28,7 +38,7 @@ func main() {
 	stsClient := sts.NewFromConfig(cfg)
 	jwtGetter := IdentityTokenRetriever{}
 	webIdentityRoleProvider := stscreds.NewWebIdentityRoleProvider(stsClient, os.Getenv("AWS_ROLE_ARN"), jwtGetter, func(o *stscreds.WebIdentityRoleOptions) {
-		o.Duration = time.Duration(12) * time.Hour
+		o.Duration = time.Duration(t) * time.Hour
 	})
 	credentials, err := webIdentityRoleProvider.Retrieve(context.TODO())
 	if err != nil {
